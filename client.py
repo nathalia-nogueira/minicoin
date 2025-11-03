23816faa472c6bf6699872f33c2cd48a7f834b1b
Bianca <biafran0712@gmail.com>
2025-11-03 15:20:56 -0300

    melhorias e comentários

--- client.py
+++ client.py
@@ -3,43 +3,51 @@ import sys
 
 def run_client():
 
+    # Verificação dos parâmetros
     if len(sys.argv) != 4:
         print("Uso correto: <nome-servidor> <porta> <dados>")
         exit(1)
 
+    # Obtêm endereço IP do servidor
     try:
-        host = sys.argv[1]
-        server_ip = socket.gethostbyname(host)
+        server_host = sys.argv[1] # Nome do servidor
+        server_ip = socket.gethostbyname(server_host)
     except socket.gaierror as e:
         print(f"Falha ao obter o endereco IP do servidor. Erro: {e}")
         exit(1)
 
+    # Porta do servidor
     server_port = int(sys.argv[2])  
     
+    # Cria socket do cliente
     try:
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except OSError as e:
         print(f"Falha ao abrir o socket. Erro: {e}")
         exit(1)
     
+    # Estabelece conexão com o servidor 
     try:
         client.connect((server_ip, server_port))
     except (socket.timeout, ConnectionRefusedError, OSError) as e:
         print(f"Falha ao conectar ao servidor. Erro: {e}")
         exit(1)
 
+    # Codifica mensagem recebida como parâmetro
     msg = sys.argv[3].encode("utf-8")
 
+    # Envia mensagem
     try:
         client.sendall(msg)
     except OSError as e:
         print(f"Erro ao enviar dados. Erro: {e}")
         exit(1)
 
-    response = client.recv(8192)
-    response = response.decode("utf-8")
+    # Resposta do servidor
+    response = client.recv(8192).decode("utf-8")
     print(f"Client received: {response}")
 
+    # Fecha socket 
     client.close()
     exit(0)
 
--- server.py
+++ server.py
@@ -1,52 +1,61 @@
 import socket
 import sys
 
-TAMFILA = 5
+QUEUESIZE = 5
 
 def run_server():
 
+    # Verificação dos parametros
     if len(sys.argv) != 2:
         print("Uso correto: <porta>")
         exit(1)
 
+    # Obtêm seu próprio endereço IP 
     try:
         server_ip = socket.gethostname()
     except OSError as e:
         print("Falha ao obter o próprio endereço IP. Erro: {e}")
         exit(1)
 
-    port = int(sys.argv[1])
+    # Porta do servidor
+    server_port = int(sys.argv[1])
 
+    # Cria socket do servidor
     try:
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except OSError as e:
         print(f"Falha ao abrir o socket. Erro: {e}")
         exit(1)
 
+    # Realiza bind do servidor
     try:
-        server.bind((server_ip, port))
+        server.bind((server_ip, server_port))
     except OSError as e:
         print(f"Falha ao fazer o bind. Erro: {e}")
         exit(1)
 
-    server.listen(TAMFILA)
-    print(f"Listening on {server_ip}:{port}")
+    # Coloca servidor em modo escuta
+    server.listen(QUEUESIZE)
+    print(f"Listening on {server_ip}:{server_port}")
 
     while True:
 
+        # Espera tentativa de conexão
         try: 
             client_socket, client_address = server.accept()
         except OSError as e:
             print(f"Falha ao estabelecer a conexão. Erro: {e}")
+        except KeyboardInterrupt:
+            exit(0)
 
-        request = client_socket.recv(8192)
-        request = request.decode("utf-8") 
-
+        # Recebe mensagem
+        request = client_socket.recv(8192).decode("utf-8") 
         print(f"Server received: {request}")
 
-        response = request.encode("utf-8") 
-        client_socket.sendall(response)
+        # Envia resposta 
+        client_socket.sendall(request.encode("utf-8"))
 
+        # Fecha socket criado pra conexão com o cliente
         client_socket.close()
 
 
